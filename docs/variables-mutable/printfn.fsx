d8f0433402f6b56bc0bfe546252a783d412142aa
Koras02 <wjdgh0727@gmail.com>
2025-09-02 12:17:12 +0900

    Fsharp #2. 변수와 불변성

---
+++ docs/variables-mutable/printfn.fsx
@@ -0,0 +1,7 @@
+// 변수 출력과 형식지정
+let name = "Kimetsu no yaiba: mugen castle";
+let release_date = (2025,8,22);
+let time = 155;
+let types = ["2d"; "IMax"; "4DX"; "MX4D";"Dolby Cinema"];
+
+printfn "영화: %s, 개봉일: %A, 상영 시간: %i분, 상영 타입: %A" name release_date time types;
